8d660dfc88573c6c6488dd5a0e01e3291f7ec119
Bruno Minelli <minellibruno@hotmail.com>
2022-03-22 17:35:12 -0300

    Requisito 4 concluido

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { renderWithRouter } from '../renderWithRouter';
+import { FavoritePokemons } from '../components';
+
+describe('Teste o componente <FavoritePokemons.js />', () => {
+  it('1- Se é exibido na tela a mensagem No favorite pokemon found.', () => {
+    renderWithRouter(<FavoritePokemons />);
+  });
+
+  it('2- Se são exibidos todos os cards de pokémons favoritados.', () => {
+    renderWithRouter(<FavoritePokemons />);
+  });
+});
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { NotFound } from '../components';
+import { renderWithRouter } from '../renderWithRouter';
+
+describe('Teste o componente <NotFound.js />', () => {
+  it('1- Se página contém um heading h2 com o texto Page requested not found.',
+    () => {
+      renderWithRouter(<NotFound />);
+      const elementText = screen
+        .getByRole('heading', { name: /Page requested not found/, level: 2 });
+      const elementEmoji = screen.getByRole('img', { name: /Crying emoji/ });
+      expect(elementText).toBeInTheDocument();
+      expect(elementEmoji).toBeInTheDocument();
+    });
+
+  it('2- Se página mostra a imagem.', () => {
+    renderWithRouter(<NotFound />);
+    const element = screen
+      .getByAltText('Pikachu crying because the page requested was not found');
+    expect(element.src).toContain('kNSeTs31XBZ3G');
+  });
+});
